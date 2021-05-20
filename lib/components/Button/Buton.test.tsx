53f0c0598ba16557c7ac1e4fd8f2fea226604905
Erick Sarabia <erick.sarabia@outlook.com>
2021-05-20 15:15:35 -0500

    Add button test with error

---
+++ lib/components/Button/Buton.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "../../theming";
+import Button from "./Button";
+
+describe("<Button />", () => {
+    test("It should have a label", () => {
+        render(
+            <ThemeProvider>
+                <Button label="button" />
+            </ThemeProvider>,
+        );
+        const BUTTON = screen.getByText("");
+        expect(BUTTON).toBeInTheDocument;
+    });
+});
